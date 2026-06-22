97de04492ff21bea91ad75710aca6f364c87b0bc
Ahmad Abdullah <abdullahhrajput4943@gmail.com>
2026-06-22 11:38:00 +0500

    Adding Favourite To Shoulders Screen Layout

--- lib/Presentation/pages/setting/favorites_page.dart
+++ lib/Presentation/pages/setting/favorites_page.dart
@@ -4,6 +4,13 @@ import 'package:get_fit/Utils/constants.dart';
 import 'package:get_fit/Presentation/pages/setting/profile_page.dart';
 import 'package:get_fit/Presentation/pages/setting/notifications_page.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get_fit/Domain/models/exercise_model.dart';
+import 'package:get_fit/Presentation/pages/gym/gym_exercises/arms/arms_exercise_detail.dart';
+import 'package:get_fit/Presentation/pages/gym/gym_exercises/back/back_exercise_detail.dart';
+import 'package:get_fit/Presentation/pages/gym/gym_exercises/chest/chest_exercise_detail.dart';
+import 'package:get_fit/Presentation/pages/gym/gym_exercises/core/core_exercise_detail.dart';
+import 'package:get_fit/Presentation/pages/gym/gym_exercises/legs/legs_exercise_detail.dart';
+import 'package:get_fit/Presentation/pages/gym/gym_exercises/shoulders/shoulders_exercise_detail.dart';
 
 class FavoritesPage extends StatefulWidget {
   const FavoritesPage({super.key});
@@ -243,9 +250,53 @@ class _FavoritesPageState extends State<FavoritesPage> {
     );
   }
 
+  void _navigateToDetail(BuildContext context, Exercise exercise) {
+    switch (exercise.category) {
+      case 'Arms':
+        Navigator.push(context, MaterialPageRoute(
+          builder: (_) => ArmsExerciseDetail(exercise: exercise)));
+        break;
+      case 'Back':
+        Navigator.push(context, MaterialPageRoute(
+          builder: (_) => BackExerciseDetail(exercise: exercise)));
+        break;
+      case 'Chest':
+        Navigator.push(context, MaterialPageRoute(
+          builder: (_) => ChestExerciseDetail(exercise: exercise)));
+        break;
+      case 'Core':
+        Navigator.push(context, MaterialPageRoute(
+          builder: (_) => CoreExerciseDetail(exercise: exercise)));
+        break;
+      case 'Legs':
+        Navigator.push(context, MaterialPageRoute(
+          builder: (_) => LegsExerciseDetail(exercise: exercise)));
+        break;
+      case 'Shoulders':
+        Navigator.push(context, MaterialPageRoute(
+          builder: (_) => ShouldersExerciseDetail(exercise: exercise)));
+        break;
+    }
+  }
+
  Widget _buildCard(BuildContext context, Map<String, dynamic> item) {
   final isDark = context.isDark;
-  return Container(
+  return GestureDetector(
+    onTap: () {
+      final exercise = Exercise(
+        id: item['exercise_id'] ?? '',
+        title: item['exercise_title'] ?? '',
+        category: item['exercise_category'] ?? '',
+        description: item['exercise_description'] ?? '',
+        imageUrl: item['exercise_image'] ?? '',
+        sets: item['exercise_sets'] ?? '',
+        reps: item['exercise_reps'] ?? '',
+        rest: item['exercise_rest'] ?? '',
+        level: item['exercise_level'] ?? '',
+      );
+      _navigateToDetail(context, exercise);
+    },
+    child: Container(
     margin: const EdgeInsets.only(bottom: 16),
     height: 130,
     decoration: BoxDecoration(
@@ -373,6 +424,7 @@ class _FavoritesPageState extends State<FavoritesPage> {
         ),
       ],
     ),
+  ),
   );
 }
 
